7af873d14f06500c3f66ab3fa5ca964c09f12b7c
carlos <carlos.blanco01@correo.usa.edu.co>
2022-09-22 21:51:21 -0500

    comento la funcion de sockets y agrego list task live

---
+++ lib/todo_app_web/live/list_task_live.ex
@@ -0,0 +1,9 @@
+defmodule TodoAppWeb.ListTaskLive do
+
+  use Phoenix.LiveView
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+end
--- lib/todo_app_web/live/simple_live.ex
+++ lib/todo_app_web/live/simple_live.ex
@@ -2,17 +2,22 @@ defmodule TodoAppWeb.SimpleLive do
 
   use Phoenix.LiveView
 
-  def render(assigns) do
-    ~H"""
-    <h1>Hello world!!!</h1>
-    <input type="text" name="input" phx-keydown="capture">
-    <label for="text">Result:</label>
-    <h3></h3>
-    """
+#  def render(assigns) do
+#    ~H"""
+#    <h1>Hello world!!!</h1>
+#    <input type="text" name="input" phx-keydown="capture">
+#    <label for="text">Result:</label>
+#    <h3><%= @message %></h3>
+#    """
+#  end
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, :message, "")
+    {:ok, socket}
   end
 
-  def handle_event("capture", params, socket) do
-    IO.inspect(params)
+  def handle_event("capture", %{"key" => _key, "value" => value}, socket) do
+    socket = assign(socket, :message, value)
     {:noreply, socket}
   end
 
